2f811d853f41a10b1e50bbc175d29c65ce229329
carlos <casanchez@nrmweb.com.mx>
2024-06-20 16:23:29 -0600

    copiando oye

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://beatdigital.mx',
-	integrations: [mdx(), sitemap()],
+  site: 'https://oyedigital.mx',
+  integrations: [mdx(), sitemap(), tailwind()],
+  devToolbar: {
+    enabled: false
+  }
 });
-
-const API_URL = import.meta.env.PUBLIC_API_URL;
-
-export async function fetchAPI( query='' ) {
-    const res = await fetch( `${API_URL}/${query}` );
-
-    if ( res.ok ) {
-        return res.json();
-    } else {
-        const error = await res.json();
-
-        throw new Error(
-            '❗ Failed to fetch API for ' + query + "\n" +
-            'Code: ' + error.code + "\n" +
-            'Message: ' + error.message + "\n"
-        );
-    }
-}
-
-export async function getArticles() {
-    const data = await fetchAPI( 'articles/?per_page=50' );
-
-    return data;
-}

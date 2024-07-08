27626cb90631ad1ce01744c5b3b780a0fcc2e953
carlos <casanchez@nrmweb.com.mx>
2024-07-08 15:42:19 -0600

    primeros ajustes beat

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://oyedigital.mx',
+  site: 'https://beatdigital.mx',
   integrations: [mdx(), sitemap(), tailwind()],
   devToolbar: {
     enabled: false

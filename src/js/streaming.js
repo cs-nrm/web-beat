7ce27b06e08f6232aa5c345215dd53a7f2ca6496
Danira <danira@MacBook-Air-de-Danira.local>
2025-08-25 12:38:45 -0600

    modal

--- src/js/streaming.js
+++ src/js/streaming.js
@@ -103,6 +103,7 @@ function initGPT() {
     var mapping6 = googletag.sizeMapping().addSize([728, 90]).build();
     var mapping61 = googletag.sizeMapping().addSize([320, 50]).build();
     var mapping14 = googletag.sizeMapping().addSize([600, 800]).build();
+    var mapping141 = googletag.sizeMapping().addSize([320, 480]).build();
          
     window.slot2 = googletag.defineSlot("/21799830913/Beat", [300, 250],'ad-slot2').defineSizeMapping(mapping2).addService(googletag.pubads());
     window.slot3 = googletag.defineSlot("/21799830913/Beat", [728, 90],'ad-slot3').defineSizeMapping(mapping3).addService(googletag.pubads());
@@ -117,6 +118,7 @@ function initGPT() {
     window.slot6 = googletag.defineSlot("/21799830913/Beat", [728, 90],'ad-slot6').defineSizeMapping(mapping6).addService(googletag.pubads());
     window.slot61 = googletag.defineSlot("/21799830913/Beat", [320, 50],'ad-slot61').defineSizeMapping(mapping61).addService(googletag.pubads());
     window.slot14 = googletag.defineSlot("/21799830913/Beat", [600, 800],'ad-slot14').defineSizeMapping(mapping14).addService(googletag.pubads());
+    window.slot141 = googletag.defineSlot("/21799830913/Beat", [320, 480],'ad-slot141').defineSizeMapping(mapping141).addService(googletag.pubads());
 
     googletag.pubads().setTargeting("test","responsive");
     googletag.enableServices();
@@ -133,6 +135,7 @@ function initGPT() {
     googletag.display('ad-slot6');
     googletag.display('ad-slot61');
     googletag.display('ad-slot14');
+    googletag.display('ad-slot141');
     
     //googletag.pubads().refresh([slot3]);
     //setInterval(function(){googletag.pubads().refresh([slot3]);}, 180000);

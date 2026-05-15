ac7e1616d7932dc5dba51114a6b949534110a5e1
Csanchez <casanchez@nrmweb.com.mx>
2026-05-15 08:23:39 -0600

    descripcion del sitio

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Beat 100.9 FM';
-export const SITE_DESCRIPTION = 'Total Music';
+export const SITE_DESCRIPTION = 'El espacio dedicado a la mejor MÚSICA ELECTRÓNICA en Mexico y el mundo';
